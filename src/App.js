a1ac3ae10907aaca277105c6a82300e87cbf5e4e
C-Lee-Hamilton <122707378+C-Lee-Hamilton@users.noreply.github.com>
2024-04-04 21:26:37 -0500

    first and last commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React, { useState } from "react";
 
 function App() {
+  const [addressObj, setAddressObj] = useState({});
+
+  const address = [
+    addressObj.city,
+    addressObj.state,
+    addressObj.country,
+    addressObj.zip,
+  ]
+    .filter(Boolean)
+    .join(", ");
+
+  const apiKey = process.env.REACT_APP_API_KEY;
+
+  const geoFinder = async (lat, lon) => {
+    try {
+      const response = await fetch(
+        `https://api.geocod.io/v1.7/reverse?q=${lat},${lon}&api_key=${apiKey}`
+      );
+      const data = await response.json();
+      setAddressObj({
+        city: data.results[0].address_components.city,
+        state: data.results[0].address_components.state,
+        country: data.results[0].address_components.country,
+        zip: data.results[0].address_components.zip,
+      });
+    } catch (error) {
+      console.error("Error fetching address:", error);
+    }
+  };
+
+  const getLocation = () => {
+    navigator.geolocation.getCurrentPosition((position) => {
+      geoFinder(position.coords.latitude, position.coords.longitude);
+    });
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={getLocation}>Get Location</button>
+      <h2>{address}</h2>
     </div>
   );
 }
